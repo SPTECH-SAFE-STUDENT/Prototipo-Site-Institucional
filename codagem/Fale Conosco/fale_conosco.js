55e51ed1458a740421b0cde8b2bbfcc6736c052e
LucCancela <lucas.cancela@sptech.school>
2024-04-12 08:15:14 -0300

    Tela Fale-Conosco - Primeira versão

---
+++ codagem/Fale Conosco/fale_conosco.js
@@ -0,0 +1,25 @@
+// function enviarMensagem(){
+
+//     const nome = input_nomeFaleConosco.value
+//     const email = input_emailFaleConosco.value
+//     const mensagem = textarea_mensagemFaleConosco.value
+
+
+// }
+
+
+// const btnMobile = document.getElementById('btn-mobile');
+
+
+// function toggleMenu(){
+    
+//     const nav = document.getElementById('navResponsivo');
+    
+//     // toglle siginfica adicione caso não tenha e remova caso tenha
+//     nav.classList.toggle('active');
+    
+//     btnMobile.addEventListener('click', swapper,toggleMenu);
+    
+// }
+
+
